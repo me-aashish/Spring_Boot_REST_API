ec1f8c1b045ea2745431d9ecf65db85b6029f652
Aashish Kumar Jha <20bcs3872@cuchd.in>
2024-02-01 17:04:10 +0530

    build GET rest api to get all surveys

---
+++ src/main/java/com/aashish/springboot/firstspringbootrestapi/survey/SurveyResource.java
@@ -0,0 +1,22 @@
+package com.aashish.springboot.firstspringbootrestapi.survey;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SurveyResource {
+
+	private SurveyService surveyService;
+
+	public SurveyResource(SurveyService surveyService) {
+		super();
+		this.surveyService = surveyService;
+	}
+
+	@RequestMapping("/surveys")
+	public List<Survey> retrieveAllSurveys() {
+		return surveyService.retrieveAllSurveys();
+	}
+}
